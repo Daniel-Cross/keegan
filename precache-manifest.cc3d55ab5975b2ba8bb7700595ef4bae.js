7d5baa5664a997caea8fe22ebe36671c1933d464
Daniel Cross <crossy1686@gmail.com>
2019-03-16 21:36:19 +0100

    Updates

---
+++ precache-manifest.cc3d55ab5975b2ba8bb7700595ef4bae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1a30de54bfc3d1481ef",
+    "url": "/keegan/static/css/main.65ecc87a.chunk.css"
+  },
+  {
+    "revision": "a1a30de54bfc3d1481ef",
+    "url": "/keegan/static/js/main.a1a30de5.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/keegan/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "bca745d0b54bc9fbc464",
+    "url": "/keegan/static/js/runtime~main.bca745d0.js"
+  },
+  {
+    "revision": "7a460e8ebb3e9ba49fc050a7583d84ad",
+    "url": "/keegan/index.html"
+  }
+];
