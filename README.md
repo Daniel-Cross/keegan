d1a1e2b2bcdd28a14cca75aba77715794b714b85
Daniel Cross <crossy1686@gmail.com>
2019-03-18 22:26:24 +0100

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Kevin Keegan Story Generator
-
-This is a React app that generates a random true story about Kevin Keegan, possibly the unluckiest man in the world...
---
+++ precache-manifest.30aa2968dc88b09f5590dd81d1327e41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b168ab2e784d4b2fa7b",
+    "url": "/static/css/main.65ecc87a.chunk.css"
+  },
+  {
+    "revision": "7b168ab2e784d4b2fa7b",
+    "url": "/static/js/main.7b168ab2.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ef231f768807d66939cd143ce7a378b4",
+    "url": "/index.html"
+  }
+];
